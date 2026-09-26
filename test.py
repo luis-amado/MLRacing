a95218b07d6ac1f2202207316954ec1d9920db01
Luis <lamadoalvarez@gmail.com>
2026-09-25 21:47:39 -0600

    Track side and grass

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import pygame
 from pygame.locals import *
 from car import Car
 import numpy as np
+import math
 
 size = (1280, 720)
+track_color = (0,0,0,255)
+side_color = 0x666666
+grass_color = 0x145c20
 
 pygame.init()
 screen = pygame.display.set_mode(size)
@@ -11,18 +15,36 @@ pygame.display.set_caption("")
 clock = pygame.time.Clock()
 running = True
 
-track_pixels = pygame.Surface(size)
-track_pixels.fill(0x666666)
+track_surface = pygame.Surface(size).convert_alpha()
+map_surface = pygame.Surface(size)
+
+def clear_map():
+  track_surface.fill((0,0,0,0))
+  map_surface.fill(grass_color)
+clear_map()
 
 clicked = False
 last_pos = None
-brush_size = 60
+track_size = 100
+side_size = 20
 
 font = pygame.font.SysFont("Arial", 20)
 
 mode = "DRAWING"
 
 car = Car()
+
+def draw_track_segment(pos):
+  pygame.draw.circle(track_surface, track_color, pos, track_size / 2)
+  pygame.draw.circle(map_surface, side_color, pos, track_size / 2 + side_size)
+
+def draw_track(pos, last_pos):
+  if last_pos is None:
+    draw_track_segment(pos)
+  else:
+    while (last_pos.distance_squared_to(curr_pos) > 1):
+      draw_track_segment(last_pos)
+      last_pos = last_pos.move_towards(curr_pos, 1)
 
 while running:
   for event in pygame.event.get():
@@ -31,32 +53,26 @@ while running:
     if event.type == pygame.KEYDOWN:
       if event.unicode == "1": mode = "DRAWING"
       elif event.unicode == "2": 
+        map_surface.blit(track_surface, (0,0))
         mode = "PLACING_CAR"
-        # also convert map (add danger zone)
 
   if mode == "DRAWING":
     if pygame.key.get_pressed()[ord('c')]:
-      track_pixels.fill(0x666666)
+      clear_map()
 
     if pygame.mouse.get_pressed()[0]:
       curr_pos = pygame.Vector2(pygame.mouse.get_pos())
-
-      if last_pos is None:
-        pygame.draw.circle(track_pixels, "black", curr_pos, brush_size)
-      else:
-        while (last_pos.distance_squared_to(curr_pos) > 2):
-          pygame.draw.circle(track_pixels, "black", last_pos, brush_size)
-          last_pos = last_pos.move_towards(curr_pos, 1)
-
+      draw_track(curr_pos, last_pos)
       last_pos = curr_pos
     else:
       last_pos = None
 
-    screen.blit(track_pixels, (0,0))
+    map_surface.blit(track_surface, (0,0))
+    screen.blit(map_surface, (0,0))
   elif mode == "PLACING_CAR":
     car.speed = 0
     car.angular_speed = 0
-    screen.blit(track_pixels, (0,0))
+    screen.blit(map_surface, (0,0))
 
     car.pos = pygame.mouse.get_pos()
     car.draw(screen)
@@ -64,15 +80,16 @@ while running:
       mode = "ROTATING_CAR"
 
   elif mode == "ROTATING_CAR":
-    screen.blit(track_pixels, (0,0))
+    screen.blit(map_surface, (0,0))
 
     if not pygame.mouse.get_pressed()[0]:
       mode = "CAR_PLACED"
-    car.rotate_to_face(pygame.mouse.get_pos())
-    car.draw(screen)
+    else:
+      car.rotate_to_face(pygame.mouse.get_pos())
+      car.draw(screen)
 
   elif mode == "CAR_PLACED":
-    screen.blit(track_pixels, (0,0))
+    screen.blit(map_surface, (0,0))
     car.update()
     car.draw(screen)
 
